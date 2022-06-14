b0397ca34083428b08b954bb275d17fa4a915b49
koalanis <kalebalanisdev@gmail.com>
2022-06-14 14:14:56 -0500

    responsive text

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       <>
         <div className="grid place-items-center h-screen bg-violet-100">
             <div className="flex justify-center content-center">
-                <h1 className="text-8xl">
+                <h1 className="text-xl sm:text-7xl">
                     {"CODE"}{"("}<span className="font-semibold text-blue-800">Y</span>{")"}{"("}<span className="font-semibold text-red-800">X</span>{")"} 
                 </h1>
             </div>
